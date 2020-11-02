62d140661224909aa2cdcf0917c713df24351ba0
Ricky Bobby <richard.pajewski@gmail.com>
2020-11-01 18:01:56 -0600

    added license copywright

--- utils/generate-markdown.js
+++ utils/generate-markdown.js
@@ -79,6 +79,28 @@ ${data.projectTests}`
     }
 }
 
+const licenseBadgeInput = data => {
+    if (data.license) {
+        // parse input
+        const licenseBadgeArr = data.license.map(v => v.toLowerCase());
+        // temp array to hold badges
+        const licenseBadgeInputArr = [];
+
+        licenseBadgeArr.forEach(function(license) {
+            if (license === 'mit') {
+                licenseBadgeInputArr.push(`![mit](https://img.shields.io/badge/license-MIT-green)`);
+            }
+            if (license === 'isc') {
+                licenseBadgeInputArr.push(`![isc](https://img.shields.io/badge/license-ISC-green)`);
+            }
+            if (license === 'apache') {
+                licenseBadgeInputArr.push(`![apache](https://img.shields.io/badge/license-Apache-green)`);
+            }
+        })
+        return licenseBadgeInputArr
+    }
+}
+
 const licenseInput = data => {
     if (data.license) {
         // parse input
@@ -90,17 +112,59 @@ const licenseInput = data => {
 `)
         licenseArr.forEach(function(license) {
             if (license === 'mit') {
-                licenseInputArr.push(`![mit](https://img.shields.io/badge/license-MIT-green)`);
+                licenseInputArr.push(`Copyright (c)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+                
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+                
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`);
             }
             if (license === 'isc') {
-                licenseInputArr.push(`![isc](https://img.shields.io/badge/license-ISC-green)`);
+                licenseInputArr.push(`Copyright
+
+Permission to use, copy, modify, and/or distribute this software for any purpose 
+with or without fee is hereby granted, provided that the above copyright notice 
+and this permission notice appear in all copies.
+                
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH 
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, 
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS 
+OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER 
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 
+OF THIS SOFTWARE.`);
             }
             if (license === 'apache') {
-                licenseInputArr.push(`![apache](https://img.shields.io/badge/license-Apache-green)`);
+                licenseInputArr.push(`Copyright
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+                
+    http://www.apache.org/licenses/LICENSE-2.0
+                
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`);
             }
         })
         licenseInputArr.push('\n')
-        return licenseInputArr.join('\n')
+        return licenseInputArr
     }
 }
 
@@ -145,7 +209,7 @@ module.exports = markdownPage => {
     // destructure user input for each section
     const { projectTitle, projectDescription, github, email, licenses, ...rest } = markdownPage;
 
-    return `# ${projectTitle}
+    return `# ${projectTitle} ${licenseBadgeInput(rest)}
 
 ## Description
 ${projectDescription}
